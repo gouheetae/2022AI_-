21d690eb54e3461d48d870fb8ab5b8d3eadb160e
gouheetae <116706056+gouheetae@users.noreply.github.com>
2022-10-29 19:13:37 +0900

    Update 챗봇.py

--- 챗봇.py
+++ 챗봇.py
@@ -24,14 +24,14 @@ st.markdown(
     f"""
     <style>
     .stApp {{
-             background-image:'bg.jpg';
+             background-image:'bg.jpg'ㅊ;
              background-attachment: fixed;
              background-size: cover
     }}
     </style>
     """,
     unsafe_allow_html=True)
-#background-image: url("C:\Users\user\Downloads\image-removebg-preview-_2_.jpg");
+#background-image: url("https://pixabay.com/ko/vectors/%ea%b3%a0%eb%9e%98-%eb%8f%99%eb%ac%bc-%ec%83%9d%ec%84%a0-%eb%b0%94%eb%8b%a4-%ec%83%9d%ed%99%9c-158438/.jpg");
 st.markdown("울산경의고등학교 코드포스에서 만든 울산을 홍보하기 워한 챗봇입니다!")
 st.markdown("울산의 여러 관광지들을 알려드립니다!")
 st.markdown('[울산경의고등학교](http://www.gyeongui.hs.kr)')
