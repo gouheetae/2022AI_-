a606024a7a4c9d7a3e57e44da7b906c4a71159db
gouheetae <116706056+gouheetae@users.noreply.github.com>
2022-12-07 10:07:41 +0900

    Update 챗봇.py

--- 챗봇.py
+++ 챗봇.py
@@ -19,9 +19,8 @@ def get_dataset():
 model = cached_model()
 df = get_dataset()
 
-st.header('울산 되살리기 프로젝트')
-#background-image: url("https://pixabay.com/ko/vectors/%ea%b3%a0%eb%9e%98-%eb%8f%99%eb%ac%bc-%ec%83%9d%ec%84%a0-%eb%b0%94%eb%8b%a4-%ec%83%9d%ed%99%9c-158438/.jpg");
-st.markdown("울산경의고등학교 코드포스에서 만든 울산을 홍보하기 워한 챗봇입니다!")
+st.header('내신 등급 분석 프로그램')
+st.markdown("10302 구희태")
 st.markdown("울산의 여러 관광지들을 알려드립니다!")
 st.markdown('[울산경의고등학교](http://www.gyeongui.hs.kr)')
 
@@ -36,10 +35,10 @@ with st.form('form', clear_on_submit=True):
     submitted = st.form_submit_button('클릭') #전송 버튼 제작
 
 if submitted and user_input:
-    embedding = model.encode(user_input)
+    embedding = model.encode(user_input) # 유저가 넣은 질문을 임배딩함
 
-    df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
-    answer = df.loc[df['distance'].idxmax()]
+    df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())  #임배딩한 질문과 가장 비슷한 질문을 찾음
+    answer = df.loc[df['distance'].idxmax()] #가장 비슷한 질문의 대답을 변수에 저장함
 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(answer['챗봇/대답'])
